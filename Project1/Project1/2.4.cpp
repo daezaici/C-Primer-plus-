dce8ef72130e53d28fe3a0b634b23244ea119ec8
大鹅在此 <121592169+daezaici@users.noreply.github.com>
2025-02-09 11:41:02 +0800

    Add files via upload

---
+++ Project1/Project1/2.4.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//#include <cmath>
+//int main()
+//{
+//	using namespace std;
+//	double area;
+//	cout << "enter the floor area.in suquare feet.of your home:"; 
+//	cin >> area;
+//	double side;
+//	side=sqrt(area);
+//	cout << "that's the equivalent of a square" << side << "feet to the side." << endl;
+//	cout << "how fascinating!" << endl;
+//
+//	return 0;
+//}
---
+++ Project1/Project1/3.1.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//#include <climits>
+//
+//int main()
+//{
+//	using namespace std;
+//	int n_int = INT_MAX;
+//	short n_short = SHRT_MAX;
+//	long n_long = LONG_MAX;
+//	long long n_llong = LLONG_MAX;
+//
+//	cout << "int is " << sizeof(int) << " bytes." << endl;
+//	cout << "short is " << sizeof(short) << " bytes." << endl;
+//	cout << "long  is " << sizeof(long) << " bytes." << endl;
+//	cout << "llong is " << sizeof(long long) << " bytes." << endl;
+//
+//	cout << "Maximum values:" << endl;
+//	cout << "int:" << n_int << endl;
+//	cout << "short:" << n_short << endl;
+//	cout << "long:" << n_long << endl;
+//	cout << "llong:" << n_llong << endl;
+//	return 0;
+//}
