bf2b13f2ebbc1a3f12075feb8d0a6409f9dfbfa7
大鹅在此 <121592169+daezaici@users.noreply.github.com>
2025-02-14 23:13:17 +0800

    Add files via upload

---
+++ Project1/Project1/5.4.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//const int ArSize = 16;
+//int main()
+//{
+//	using namespace std;
+//	long long a[ArSize];
+//	a[1] = a[0] = 1LL;
+//	for (int i = 2;i < ArSize;i++)
+//	{
+//		a[i] = a[i - 1] * i;
+//	}
+//	for (int i = 0;i < ArSize;i++)
+//	{
+//		cout << i << "! = " << a[i] << endl;;
+//	}
+//
+//	return 0;
+//}
